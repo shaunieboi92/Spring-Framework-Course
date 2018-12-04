f1d3c3f7b1108d3e8804f9b16ec26d285d905ec4
shaunieboi92 <shau0004@e.ntu.edu.sg>
2018-12-04 15:39:42 +0800

    Add files via upload
    Init and Destroy, one with annotation method, one with xml config method

--- InitandDestroyMethodson/Fruit2.java
+++ InitandDestroyMethodson/Fruit2.java
@@ -1,19 +1,21 @@
 package InitandDestroyMethodson;
 
-public class Fruit {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+public class Fruit2 {
 	//set default description as not set
 	private String description = "not set";
 
 	public String getDescription() {
 		return description;
 	}
-
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
 	
 	//create no argument constructor
-	public Fruit () {}
+	public Fruit2 () {}
 	
 	public String talkAboutYourSelf() {
 		return description;
@@ -24,12 +26,15 @@ public class Fruit {
 	
 	//these initMethod() and destroyMethod() will be referenced in the appContext
 	//it will be set automatically before and after the main called methods
+	
+	@PostConstruct
 	public void initMethod() {
-		System.out.println("The fruit bean is ready to go.");
+		System.out.println("The fruit bean is ready to go, this is with init annotation.");
 	}
+	@PreDestroy
 	//destroy method will be shown right before the close
 	public void destroyMethod() {
-		System.out.println("The fruit bean is about to be destroyed. ");
+		System.out.println("The fruit bean is about to be destroyed, this is with destroy annotation. ");
 	}
 	
 }
--- InitandDestroyMethodson/myApp.java
+++ InitandDestroyMethodson/myApp.java
@@ -8,13 +8,19 @@ public class myApp {
 	public static void main (String args[] ) {
 		
 		ApplicationContext appContext5 = new ClassPathXmlApplicationContext("myTestPackage/appContext5.xml");
-		
+		ApplicationContext appContext12 = new ClassPathXmlApplicationContext("myTestPackage/appContext12.xml");
+
 
 		Fruit myFruit = appContext5.getBean("fruitwithDestroyer", Fruit.class);
+		Fruit2 anotherFruit = appContext12.getBean("fruitwithDestroyer", Fruit2.class);
 
 		System.out.println(myFruit.talkAboutYourSelf());
+		System.out.println(anotherFruit.talkAboutYourSelf());
+
 		//close spring framework
 		
 		((ClassPathXmlApplicationContext)appContext5).close();
+		((ClassPathXmlApplicationContext)appContext12).close();
+
 	}
 }
